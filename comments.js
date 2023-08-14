f09ad6baae60082b468fbceaa5029f1e5b2bfd32
meyerc-dev <142184695+meyerc-dev@users.noreply.github.com>
2023-08-14 13:39:06 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,65 @@
+// Create web server 
+// 1. Import express
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const authenticate = require('../authenticate');
+const cors = require('./cors');
+
+// 2. Create router
+const commentRouter = express.Router();
+commentRouter.use(bodyParser.json());
+
+// 3. Import the model
+const Comments = require('../models/comments');
+
+// 4. Create endpoints
+commentRouter.route('/')
+.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+.get(cors.cors, (req, res, next) => { // Get all comments
+    Comments.find(req.query)
+    .populate('author')
+    .then((comments) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(comments);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => { // Create new comment
+    if (req.body != null){
+        req.body.author = req.user._id;
+        Comments.create(req.body)
+        .then((comment) => {
+            Comments.findById(comment._id)
+            .populate('author')
+            .then((comment) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(comment);
+            })
+        }, (err) => next(err))
+        .catch((err) => next(err));
+    }
+    else {
+        err = new Error('Comment not found in request body');
+        err.status = 404;
+        return next(err);
+    }
+})
+.put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => { // Update all comments
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /comments');
+})
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => { // Delete all comments
+    Comments.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp); 
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+commentRouter.route('/:commentId')
+.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200);
